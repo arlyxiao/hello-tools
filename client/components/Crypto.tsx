8c86547042e647ab4f3f873af1e199b1fa8c004d
arlyxiao <kingla_pei@163.com>
2021-05-29 06:17:47 +0800

    Add base64 and sha256

--- client/components/Crypto.tsx
+++ client/components/Crypto.tsx
@@ -1,14 +1,27 @@
 
 import React, { useState, useRef } from "react";
 import md5 from "crypto-js/md5";
+import sha256 from 'crypto-js/sha256';
 
 
 const Crypto = () => {
   const [inputValue, setInputValue] = useState("");
-  const [md5Value, setMd5Value] = useState("");
+  const [resultValue, setResultValue] = useState("");
 
-  function convert() {
-    setMd5Value(md5(inputValue).toString());
+  function convertMd5() {
+    setResultValue(md5(inputValue).toString());
+  }
+
+  function convertSha256() {
+    setResultValue(sha256(inputValue).toString());
+  }
+
+  function encodeBase64() {
+    setResultValue(btoa(inputValue));
+  }
+
+  function decodeBase64() {
+    setResultValue(atob(inputValue));
   }
 
   function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
@@ -22,10 +35,13 @@ const Crypto = () => {
                   onChange={ (event: React.ChangeEvent<HTMLTextAreaElement>) => handleChange(event) }></textarea>
 
         <div className="action-list">
-          <button type="button" onClick={() => convert()}>md5</button>
+          <button type="button" onClick={() => convertMd5()}>Md5</button>
+          <button type="button" onClick={() => convertSha256()}>Sha256</button>
+          <button type="button" onClick={() => encodeBase64()}>Encode Base64</button>
+          <button type="button" onClick={() => decodeBase64()}>Decode Base64</button>
         </div>
 
-        <textarea name="result" defaultValue={md5Value}></textarea>
+        <textarea name="result" readOnly defaultValue={resultValue}></textarea>
       </div>
     </>
   )
