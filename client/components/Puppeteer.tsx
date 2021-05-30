51b3103bc80658200b88a3692868d66fdaea10be
arlyxiao <kingla_pei@163.com>
2021-05-30 12:16:06 +0800

    Improve UI

--- client/components/Puppeteer.tsx
+++ client/components/Puppeteer.tsx
@@ -5,6 +5,7 @@ import React, { useState, useRef } from "react";
 const Puppeteer = () => {
   const [inputValue, setInputValue] = useState("https://baidu.com");
   const [result, setResult] = useState("");
+  let timer: any;
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setInputValue(event.target.value);
@@ -14,21 +15,27 @@ const Puppeteer = () => {
     const url = `http://localhost:4500/html-to-pdf?url=${inputValue}`;
 
     setResult("http://localhost:4500/static/loading.html");
-    fetch(url)
-      .then( res => res.blob() )
-      .then( blob => {
-        var file = window.URL.createObjectURL(blob);
-        setResult(file);
-      });
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+      fetch(url)
+        .then( res => res.blob() )
+        .then( blob => {
+          const file = window.URL.createObjectURL(blob);
+          setResult(file);
+        });
+    }, 500);
+    
   }
 
   return (
     <>
       <div className="puppeteer panel">
-        <input type="text"
-               name=""
-               value={inputValue}
-               onChange={ (event: React.ChangeEvent<HTMLInputElement>) => handleChange(event) } />
+        <div className="input-wrapper">
+          <input type="text"
+                name=""
+                value={inputValue}
+                onChange={ (event: React.ChangeEvent<HTMLInputElement>) => handleChange(event) } />
+        </div>
 
         <button type="button"
                 onClick={ handleClick }>HTML To PDF</button>
