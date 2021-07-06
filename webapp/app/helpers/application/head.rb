abb49ef235c6bec39da54d71bad69c4a32d74ed8
arlyxiao <kingla_pei@163.com>
2021-07-07 07:25:54 +0800

    Improve page performance

--- webapp/app/helpers/application/head.rb
+++ webapp/app/helpers/application/head.rb
@@ -9,7 +9,19 @@ module Application
         }
       }
   
-      default = 'Best platform to show your github issues.'
+      default = 'Best platform to publish your github issues.'
+      title = data.try(:[], controller_name.to_sym).try(:[], action_name.to_sym)
+      title.blank? ? default : title
+    end
+
+    def render_meta_description
+      data = {
+        topics: {
+          show: @topic&.title
+        }
+      }
+
+      default = 'Make your github issues as your personal notes automatically.'
       title = data.try(:[], controller_name.to_sym).try(:[], action_name.to_sym)
       title.blank? ? default : title
     end
--- webapp/app/javascript/components/store/front/index.jsx
+++ webapp/app/javascript/components/store/front/index.jsx
@@ -2,9 +2,14 @@ import { createStore } from 'redux';
 
 import rootReducer from './reducers';
 
+let devTool = null;
+if (process.env.NODE_ENV === 'development') {
+  devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  devTool
 );
 
 export default store;
