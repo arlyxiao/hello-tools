8f114841cf1402ebac12a63f3ddb30dbd37e9172
arlyxiao <kingla_pei@163.com>
2021-07-07 09:05:06 +0800

    Recover devTool config

--- webapp/app/javascript/components/store/front/index.jsx
+++ webapp/app/javascript/components/store/front/index.jsx
@@ -2,10 +2,7 @@ import { createStore } from 'redux';
 
 import rootReducer from './reducers';
 
-let devTool = null;
-if (process.env.NODE_ENV === 'development') {
-  devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-}
+let devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const store = createStore(
   rootReducer,
