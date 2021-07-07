442ee14ff9b436cc43b2b718e40b4210b0b005b8
arlyxiao <kingla_pei@163.com>
2021-07-07 13:34:34 +0800

    Talk to search engine with invalid pages

--- webapp/app/controllers/application_controller.rb
+++ webapp/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  before_action :skip_invalid_page_for_search_engine
+
+  def skip_invalid_page_for_search_engine
+    error_pages = [
+      '/404',
+      '/500'
+    ]
+    if browser.bot?
+      if error_pages.include?(request.path)
+        head :moved_permanently
+      end
+    end
+  end
 end
