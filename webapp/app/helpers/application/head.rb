ad007e51aaf8c6ca844c2558d59c295bbc9d9821
arlyxiao <kingla_pei@163.com>
2021-07-19 18:21:26 +0800

    Fix head title

--- webapp/app/helpers/application/head.rb
+++ webapp/app/helpers/application/head.rb
@@ -2,6 +2,18 @@ module Application
   module Head
     extend ActiveSupport::Concern
 
+    def render_head_title
+      data = {
+        topics: {
+          show: @topic&.title
+        }
+      }
+
+      default = 'farseek.top - Best tools platform.'
+      title = data.try(:[], controller_name.to_sym).try(:[], action_name.to_sym)
+      title.blank? ? default : title
+    end
+
     def render_meta_description
       data = {
         topics: {
@@ -9,7 +21,10 @@ module Application
         }
       }
 
-      default = 'Make your github issues as your personal notes automatically.'
+      default = <<~HEREDOC.gsub(/\n/, ' ')
+        One of the best tools platform. The tools include PDF converter,
+        font converter. And you can sync your github issues on the platform.
+      HEREDOC
       title = data.try(:[], controller_name.to_sym).try(:[], action_name.to_sym)
       title.blank? ? default : title
     end
